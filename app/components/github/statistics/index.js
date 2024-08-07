06e5ced43a5d6fea04a750719b22df564e3d4cbc
Brent Hoskins <brenthoskins@Brents-Laptop.hsd1.ca.comcast.net>
2024-08-07 16:38:34 -0700

    adding to nav

--- app/components/github/statistics/index.js
+++ app/components/github/statistics/index.js
@@ -7,21 +7,22 @@ async function Statistics() {
 
   return (
     <>
-    <div className="py-8">
-      <div className="flex justify-center my-5 lg:py-8">
-        <div className="flex  items-center">
-          <span className="w-24 h-[2px] bg-[#1a1443]"></span>
-          <span className="bg-[#1a1443] w-fit text-white p-2 px-5 text-2xl rounded-md">
-            Github Statistics
-          </span>
-          <span className="w-24 h-[2px] bg-[#1a1443]"></span>
+      <div className="py-8">
+        <div className="flex justify-center my-5 lg:py-8">
+          <div className="flex  items-center">
+            <span className="w-24 h-[2px] bg-[#1a1443]"></span>
+            <span className="bg-[#1a1443] w-fit text-white p-2 px-5 text-2xl rounded-md">
+              Github Statistics
+            </span>
+            <span className="w-24 h-[2px] bg-[#1a1443]"></span>
+          </div>
         </div>
       </div>
-    </div>
 
-    <div className=" flex-wrap">
-      <div className="grow py-4 gap-4 lg:gap-8">
-        <div className="order-1 from-[#0d1224] border-[#1b2c68a0] relative rounded-lg border bg-gradient-to-r to-[#0a0d37]">
+      <div className="container mx-auto">
+
+        <div className="column-1 gap-4 lg:gap-8">
+          <div className="order-1 from-[#0d1224] border-[#1b2c68a0] relative rounded-lg border bg-gradient-to-r to-[#0a0d37]">
               <div className="flex flex-row">
                 <div className="h-[1px] w-full bg-gradient-to-r from-transparent via-pink-500 to-violet-600"></div>
                 <div className="h-[1px] w-full bg-gradient-to-r from-violet-600 to-transparent"></div>
@@ -42,11 +43,13 @@ async function Statistics() {
                     className="rounded-lg "
                   />
               </div>
+          </div>
         </div>
-      </div>
 
-      <div className="flex justify-center py-4 gap-4 lg:gap-8">
-        <div className="order-1 from-[#0d1224] border-[#1b2c68a0] relative rounded-lg border bg-gradient-to-r to-[#0a0d37] w-fit">
+        <div className="columns-1 lg:columns-2  gap-4 lg:gap-8">
+
+          <div className="pt-4 md:pt-8">
+            <div className="order-1 from-[#0d1224] border-[#1b2c68a0] relative rounded-lg border bg-gradient-to-r to-[#0a0d37] w-fit">
               <div className="flex flex-row">
                 <div className="h-[1px] w-full bg-gradient-to-r from-transparent via-pink-500 to-violet-600"></div>
                 <div className="h-[1px] w-full bg-gradient-to-r from-violet-600 to-transparent"></div>
@@ -67,9 +70,11 @@ async function Statistics() {
                     className="rounded-lg "
                   />
               </div>
-        </div>
+            </div>
+          </div>
 
-        <div className="order-2 from-[#0d1224] border-[#1b2c68a0] relative rounded-lg border bg-gradient-to-r to-[#0a0d37] w-fit">
+          <div className="pt-4 md:pt-8">
+            <div className="order-2 from-[#0d1224] border-[#1b2c68a0] relative rounded-lg border bg-gradient-to-r to-[#0a0d37] w-fit">
               <div className="flex flex-row">
                 <div className="h-[1px] w-full bg-gradient-to-r from-transparent via-pink-500 to-violet-600"></div>
                 <div className="h-[1px] w-full bg-gradient-to-r from-violet-600 to-transparent"></div>
@@ -90,10 +95,11 @@ async function Statistics() {
                     className="rounded-lg "
                   />
               </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
 
+      </div>
     </>
   );
 };
