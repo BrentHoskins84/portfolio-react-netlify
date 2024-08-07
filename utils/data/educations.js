0e9885c61c0594184e276d268139366e1341be65
Brent Hoskins <brenthoskins@Brents-Laptop.hsd1.ca.comcast.net>
2024-08-07 08:01:02 -0700

    type-o

--- utils/data/educations.js
+++ utils/data/educations.js
@@ -1,16 +1,17 @@
 export const educations = [
   {
     id: 1,
-    title: "Associate of Art in General Studies",
-    duration: "2009 - 2012",
-    institution: "Central Texas College",
-  },
-  {
-    id: 2,
     title: "Bachelor of Science in Information and Technology Management (ITM)",
     duration: "2013 - 2016",
     institution: "National University",
   },
+  {
+    id: 2,
+    title: "Associate of Art in General Studies",
+    duration: "2009 - 2012",
+    institution: "Central Texas College",
+  },
+
   {
     id: 3,
     title: "Security+",
